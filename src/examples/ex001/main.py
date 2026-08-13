a5c69659f91081a066e63d4e093cfa080b35651d
gaaiuss <caio.gui.castro@gmail.com>
2026-08-13 14:57:53 -0300

    add: env model var

--- src/examples/ex001/main.py
+++ src/examples/ex001/main.py
@@ -1,7 +1,14 @@
+import os
+
+from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
 from rich import print
 
-llm = init_chat_model("ollama:llama3.2")
+load_dotenv()
+
+model = os.getenv("MODEL", "Not working. Read the `.env-example`.")
+
+llm = init_chat_model(model)
 
 response = llm.invoke("hi, how are you?")
 print(response)
