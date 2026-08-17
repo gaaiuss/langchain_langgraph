517c077d0efa7901af60961755cdae2ab6c9bcf6
Caio Guilherme de Castro <caio.gui.castro@gmail.com>
2026-08-17 19:21:49 -0300

    upd: graph examples commented

--- src/Langraph - Graph, Edges, Nodes and State/ex003.py
+++ src/Langraph - Graph, Edges, Nodes and State/ex003.py
@@ -24,17 +24,95 @@ Minimum requirements to create a Graph:
     - Edges: node conections that can be simple or conditional.
 """
 
-from typing import TypedDict
+import operator
+from typing import Annotated, TypedDict
+
+from langgraph.graph import StateGraph
+from rich import print
 
 
 # 1. Define node state (TypedDict or dataclass)
 class State(TypedDict):
     # for each node executed, it will store the path in a list[str]
-    nodes_path: list[str]
+    # nodes_path: list[str]
+
+    """
+    For this case, the correct way is to use Annotated with what they call a
+    reducer. The reducer basically gets the output from the previous nodes and
+    join them in another state, as showed in the example function bellow.
+    """
+
+    # def reducer(a: list[str], b: list[str]) -> list[str]:
+    #     return a + b
+    # nodes_path: Annotated[list[str], reducer]
+
+    # nodes_path: Annotated[list[str], lambda a, b: a + b]
+    # In the case bellow the operator.add does the same thing as the lambda
+    # function atop.
+    nodes_path: Annotated[list[str], operator.add]
 
 
 # 2. Create the nodes
-def node_a(state: State) -> State: ...
+def node_a(state: State) -> State:
+    """
+    In the nodes you do not change the state directly, you will always return
+    a new state.
+    """
+    # nodes_path = state["nodes_path"]  # or input state
+    # output_state: State = {"nodes_path": [*nodes_path, "A"]}
+
+    # In normal circunstances we always return a new state with only the proper
+    # output from the node
+    output_state: State = {"nodes_path": ["A"]}  # we want only the return of A
+    print("> node_a", f"{state=}", f"{output_state=}")
+    return output_state
 
 
-def node_b(state: State) -> State: ...
+def node_b(state: State) -> State:
+    # nodes_path = state["nodes_path"]  # or input state
+    # output_state: State = {"nodes_path": [*nodes_path, "B"]}
+    output_state: State = {"nodes_path": ["B"]}  # we want only the return of B
+    print("> node_b", f"{state=}", f"{output_state=}")
+    return output_state
+
+
+# 3. Define graph builder
+builder = StateGraph(State)
+
+builder.add_node("A", node_a)
+builder.add_node("B", node_b)
+
+# 4. Create the edges
+builder.add_edge("__start__", "A")  # conects start to node_a
+builder.add_edge("A", "B")  # conects node_a to node_b
+builder.add_edge("B", "__end__")  # conects node_b to end
+
+
+# 5. Compile the graph
+graph = builder.compile()
+
+# Generates the graph as PNG
+# graph.get_graph().draw_mermaid_png(output_file_path="graph.png")
+
+# Get result
+
+"""
+The invoke function needs the state, in this example we need to follow the
+State(TypedDict) signature, in other words, we need a `nodes_path: list[str]` as
+the type of state the the invoke function needs. 
+
+You can use the class State, created before or you can pass the dictionary following
+the `nodes_path: list[str]` signature.
+"""
+# response = graph.invoke("Send message") # Wrong
+
+"""
+This invoke method pass the state (`"nodes_path": []` in this case) to the
+other nodes of the graph.
+"""
+response = graph.invoke({"nodes_path": []})  # this could be the State class
+
+# Result after the graph is all built
+print()
+print(f"{response=}")
+print()
--- src/Langraph - Graph, Edges, Nodes and State/ex004.py
+++ src/Langraph - Graph, Edges, Nodes and State/ex004.py
@@ -1,40 +1,58 @@
+"""Graph with conditional edges"""
+
 import operator
 from dataclasses import dataclass
 from typing import Annotated, Literal
 
-from langgraph.graph import StateGraph
+from langgraph.graph import END, START, StateGraph
 from rich import print
 
 
+# As a dataclass it will be the same ideia, the difference is that the dataclass
+# will help you inside the nodes but the return will be a dict as always.
 @dataclass
 class State:
     nodes_path: Annotated[list[str], operator.add]
-    current_number: int = 0
+    # This number will be used to control the state number that will be used
+    # by the conditional function `the_conditional`
+    current_state_number: int = 0
 
 
 def node_a(state: State) -> State:
-    output_state: State = State(nodes_path=["A"], current_number=state.current_number)
+    output_state: State = State(
+        nodes_path=["A"],
+        current_state_number=state.current_state_number,
+    )
     print("> node_a", f"{state=}", f"{output_state=}")
     return output_state
 
 
 def node_b(state: State) -> State:
-    output_state: State = State(nodes_path=["B"], current_number=state.current_number)
+    output_state: State = State(
+        nodes_path=["B"],
+        current_state_number=state.current_state_number,
+    )
     print("> node_b", f"{state=}", f"{output_state=}")
     return output_state
 
 
 def node_c(state: State) -> State:
-    output_state: State = State(nodes_path=["C"], current_number=state.current_number)
+    output_state: State = State(
+        nodes_path=["C"],
+        current_state_number=state.current_state_number,
+    )
     print("> node_c", f"{state=}", f"{output_state=}")
     return output_state
 
 
 # Conditional function
+# This is the function that represents the conditional edge, it returns the literal
+# node that will be called next.
 def the_conditional(state: State) -> Literal["goes_to_b", "goes_to_c"]:
-    if state.current_number >= 50:  # noqa: PLR2004
-        return "goes_to_c"
-    return "goes_to_b"
+    if state.current_state_number >= 50:  # noqa: PLR2004
+        # the name here is different because we are talking about the edges itself.
+        return "goes_to_c"  # name of the edge
+    return "goes_to_b"  # name of the edge
 
 
 builder = StateGraph(State)
@@ -43,17 +61,23 @@ builder.add_node("A", node_a)
 builder.add_node("B", node_b)
 builder.add_node("C", node_c)
 
-builder.add_edge("__start__", "A")
+builder.add_edge(START, "A")  # START: same as "__start__"
+# As for this example, after the node A we will go to B or C, so that is why we
+# use the conditional edge here.
 builder.add_conditional_edges(
-    "A",
-    the_conditional,
-    {"goes_to_b": "B", "goes_to_c": "C"},
+    "A",  # source
+    the_conditional,  # conditional function
+    # teaching the result of the condition to the conditional function
+    {
+        # the first is the edge name and the second the node name
+        "goes_to_b": "B",
+        "goes_to_c": "C",
+    },
 )
-builder.add_edge("B", "__end__")
-builder.add_edge("C", "__end__")
+builder.add_edge("B", END)  # END: same as "__end__"
+builder.add_edge("C", END)  # END: same as "__end__"
 
 graph = builder.compile()
-print(graph.get_graph().draw_mermaid_png(output_file_path="graph.png"))
 
 print()
 response = graph.invoke(State(nodes_path=[]))
@@ -62,6 +86,7 @@ print()
 
 
 print()
-response = graph.invoke(State(nodes_path=[], current_number=51))
+# Testing the condition inside the conditional function
+response = graph.invoke(State(nodes_path=[], current_state_number=51))
 print(f"{response=}")
 print()
