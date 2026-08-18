7fdffbfc3082efd74e4fc89b320990e7f0ded602
gaaiuss <caio.gui.castro@gmail.com>
2026-08-18 12:37:50 -0300

    upd: chatbot with checkpointer

--- src/Chatbot/main.py
+++ src/Chatbot/main.py
@@ -1,4 +1,17 @@
+"""Checkpointer
+
+For the purpose of not needing to save the context history hardcoded we use the
+checkpointer feature.
+
+For each interaction with the state, the checkpointer saves it in memory, a
+database, etc to send the context history to the AI.
+
+To start using the checkpointer you will need a thread id, a number to store
+the context history the is configured inside the graph invoke or creation.
+"""
+
 import os
+import threading
 from collections.abc import Sequence
 from typing import Annotated, TypedDict
 from unittest import result
@@ -7,9 +20,11 @@ from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
 from langchain.messages import HumanMessage
 from langchain_core.messages import BaseMessage
+from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph import END, START, StateGraph, add_messages
-from langgraph.graph.message import Messages
+from langgraph.graph.state import RunnableConfig
 from rich import print
+from rich.markdown import Markdown
 
 load_dotenv()
 
@@ -18,44 +33,18 @@ model = os.getenv("MODEL")
 llm = init_chat_model(model)
 
 
-# Just to debug the code
-def reducer(a: Messages, b: Messages) -> Messages:
-    return add_messages(a, b)
-
-
 # 1. Define state
-class AgentState(TypedDict):  # the state can be any name
-    # As a pattern, when you talk about messages history, we call this var
-    # messages
-    # Every time that you manipulate this field in anywhere of the graph
-    # yoy are triggering the reducer that joins the nodes returns to pass it to
-    # the next node.
+class AgentState(TypedDict):
     messages: Annotated[
-        # As we do not want the state to be changed directly, we do not want to
-        # change the messages directly so we use Sequence for ReadOnly.
         Sequence[BaseMessage],
-        reducer,  # our reducer just to trace the triggers
-        # add_messages,  # add_messages: reducer from langgraph
+        add_messages,  # add_messages: reducer from langgraph
     ]
 
 
 # 2. Create nodes
-def call_llm(state: AgentState) -> AgentState:  # this is a node
-    # Here we manipulate the 'messages' field of the state, triggering the reducer
-    # returning a new state
-    # return {"messages": [AIMessage("Hello, how are you?")]}
-
-    # When calling the llm passing a message is not ideal.
-    # llm_result = llm.invoke('llm message')
-
-    # Here we will pass
-    # ALL the messages for it to have all the history.
+def call_llm(state: AgentState) -> AgentState:
     llm_result = llm.invoke(state["messages"])
     return {"messages": [llm_result]}
-
-    # Simulating a llm result
-    # llm_result = AIMessage("Hello, how are you?")
-    # return {"messages": [llm_result]}
 
 
 # 3. Create the StateGraph or builder
@@ -70,27 +59,44 @@ builder = StateGraph(
 
 
 # 4. Add nodes to the graph
-builder.add_node(
-    # the name here can be anything but we maintain like this to make the
-    # understanding easier
-    "call_llm",
-    call_llm,
-)
+builder.add_node("call_llm", call_llm)
 
 # 5. Add edges
 builder.add_edge(START, "call_llm")
 builder.add_edge("call_llm", END)
 
+# Creating the checkpointer
+checkpointer = InMemorySaver()
+
+# Creating the config that the checkpointer needs
+# config = RunnableConfig(configurable={"thread_id": 1})  # This number can be any.
+
+# You can also get the id dinamically using threading
+config = RunnableConfig(configurable={"thread_id": threading.get_ident()})
 
 # 6. Compile graph
-graph = builder.compile()
+graph = builder.compile(checkpointer=checkpointer)
 
 
 # 7. Use graph
 if __name__ == "__main__":
-    human_message = HumanMessage("Hello, my name is Caio.")
-    result = graph.invoke(
-        # state var messages with a Sequence like a list
-        {"messages": [human_message]},
-    )
-    print(result)
+    while True:
+        user_input = input("Type your message: ")
+        print(Markdown("---"))
+
+        if user_input.lower() in ["q", "quit"]:
+            print("Bye!")
+            print(Markdown("---"))
+            break
+
+        human_message = HumanMessage(user_input)
+        result = graph.invoke(
+            {"messages": [human_message]},
+            # Here we are configuring the thread id that will be used by the
+            # checkpointer to store the message history.
+            config=config,
+        )
+
+        # get the last message, in this case from the llm
+        print(Markdown(str(result["messages"][-1].content)))
+        print(Markdown("---"))
